8b0e7a5f819c2207320a8897df586fec3b230363
Lucas Jansen <48385544+lucasrjansen@users.noreply.github.com>
2019-07-03 19:26:11 -0300

    Grud Receita Correções (#13)
    * GRUD Ingrediente
    
    * Alterações Leandro
    
    * GRUD Ingrediente
    
    * GRUD Ingrediente - Final
    
    * Correção IngredienteController
    
    * Correção IngredienteController
    
    * Validações Grud Ingrediente
    
    * GRUD receita correções

--- blucake-api/src/main/java/br/com/blucake/api/controllers/ReceitaController.java
+++ blucake-api/src/main/java/br/com/blucake/api/controllers/ReceitaController.java
@@ -10,11 +10,15 @@ import br.com.blucake.api.models.Receita;
 import br.com.blucake.api.models.Response;
 import br.com.blucake.api.models.Usuario;
 import br.com.blucake.api.services.ReceitaService;
+import br.com.blucake.api.services.UsuarioService;
+import br.com.blucake.api.services.UsuarioServiceImpl;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,13 +28,15 @@ import org.springframework.web.bind.annotation.RestController;
  *
  * @author Leandro Prado
  */
-
 @RestController
 @RequestMapping("/receitas")
 public class ReceitaController {
     
-     @Autowired
-     ReceitaService receitaService;
+    @Autowired
+    ReceitaService receitaService;
+    
+    @Autowired
+    UsuarioServiceImpl usuarioService;
     
     @GetMapping
     @PreAuthorize("hasAnyRole('ADMIN', 'USUARIO')")
@@ -41,18 +47,18 @@ public class ReceitaController {
         return ResponseEntity.ok().body(response);
     }
     
-    @GetMapping
+    @GetMapping("/{id}")
     @PreAuthorize("hasAnyRole('ADMIN', 'USUARIO')")
-    @RequestMapping("/porUsuario")
-    public ResponseEntity<Response> buscarReceitaPorUsuario(@RequestBody Usuario usu) {
-        List<Receita> lista = receitaService.buscaReceitaPorUsuario(usu);
+    public ResponseEntity<Response> buscarReceitaPorUsuario(@PathVariable String id) {
+        
+        Optional<Usuario> user = usuarioService.buscarPorId(Long.parseLong(id));
+        List<Receita> lista = receitaService.buscaReceitaPorUsuario(user.get());
         Response response = new Response(lista);
         return ResponseEntity.ok().body(response);
     }
     
     @PostMapping
     @PreAuthorize("hasAnyRole('ADMIN', 'USUARIO')")
-    @RequestMapping("/cadastro")
     public ResponseEntity<Response> cadastroDeReceita(@RequestBody Receita receita) {
         Receita recei = receitaService.cadastroDeReceita(receita);
         Response response = new Response(recei);
